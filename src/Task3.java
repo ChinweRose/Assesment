08a6ebc5b257d5dee59af271da8296d18ec611e5
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 12:05:29 +0100

    my second task

---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 3;
+        double result = (50/3);
+
+        System.out.println("50/3 is " + result);
+    }
+}
