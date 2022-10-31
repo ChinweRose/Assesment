9e1022f62b427b5137adc23ccae575bb15675875
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 12:33:42 +0100

    my fifth task

---
+++ src/Task5.java
@@ -0,0 +1,11 @@
+public class Task5 {
+    public static void main(String[] args) {
+
+        int a = 55;
+        int b = 9;
+        int c = 9;
+        int result = ((a + b) % 9);
+
+        System.out.println("Answer is " + result);
+    }
+}
