43c1af7efe9ab09dbcec218ca449f34fb54c4343
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 12:46:06 +0100

    my sixth task

---
+++ src/Task6.java
@@ -0,0 +1,12 @@
+public class Task6 {
+    public static void main(String[] args) {
+
+        int a = 55;
+        int b = 9;
+        int c = 9;
+
+        int result = ((55 + 9) % 9);
+
+        System.out.println("Result is " + result);
+    }
+}
