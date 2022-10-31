5f4197d36253b4af6ef94621e68d001870e91643
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 13:20:29 +0100

    my seventh task

---
+++ src/Task7.java
@@ -0,0 +1,14 @@
+public class Task7 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int c = 3;
+        int d = 2;
+        int e = 8;
+        int f = 3;
+
+        int result = (a + b / c * d - e % f);
+
+        System.out.println("Answer is " + result);
+    }
+}
