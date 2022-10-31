b9ee134aba5ab96644c34dd0c52a86318a3fdb56
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 12:23:28 +0100

    my second task

--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,12 @@
 public class Task4 {
     public static void main(String[] args) {
 
-//        int
+        int num1 = -5;
+        int num2 = 8;
+        int num3 = 6;
+
+        int result = (num1 + num2 * num3);
+
+        System.out.println("-5 + 8 + 6 is " + result);
     }
 }
