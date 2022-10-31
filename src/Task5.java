3820d56d9ae79a091042d9b6e0aaf3e05f3b3eaa
Chinwe <103608228+ChinweRose@users.noreply.github.com>
2022-10-31 12:41:05 +0100

    my fifth task

--- src/Task5.java
+++ src/Task5.java
@@ -1,10 +1,11 @@
 public class Task5 {
     public static void main(String[] args) {
 
-        int a = 55;
-        int b = 9;
-        int c = 9;
-        int result = ((a + b) % 9);
+        int p = 20 ;
+        int q = -3;
+        int r = 5;
+        int s = 8;
+        int result =(p + (q*r) / s);
 
         System.out.println("Answer is " + result);
     }
